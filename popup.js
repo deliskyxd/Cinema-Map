e7a138cce2ac63fd12a612281a287e6b2566e486
Voosaki <kamil.szczukowski@interia.pl>
2024-01-12 18:17:45 +0100

    Make map center on click

--- popup.js
+++ popup.js
@@ -1,35 +1,45 @@
-import {Overlay} from 'ol';
+import { Overlay } from "ol";
 
 export default function createPopup(map) {
-    const popupOverlay = new Overlay({
-      element: document.getElementById('popup'),
-      positioning: 'bottom-center',
-      stopEvent: false,
-      offset: [0, -50],
+  const popupOverlay = new Overlay({
+    element: document.getElementById("popup"),
+    positioning: "bottom-center",
+    stopEvent: false,
+    offset: [0, -20],
+  });
+
+  map.addOverlay(popupOverlay);
+
+  map.on("click", function (event) {
+    const feature = map.forEachFeatureAtPixel(event.pixel, function (feature) {
+      return feature;
     });
-  
-    map.addOverlay(popupOverlay);
-  
-    map.on('click', function (event) {
-      const feature = map.forEachFeatureAtPixel(event.pixel, function (feature) {
-        return feature;
-      });
-  
-      if (feature) {
-        const featureData = feature.getProperties();
-        console.log(featureData);
-        const content =  
-        `<p>${featureData.cinema.name}</p>
+
+    if (feature) {
+      const featureData = feature.getProperties();
+
+      const featureCoordinate = feature.getGeometry().getCoordinates();
+      const mapSize = map.getSize();
+      const centerOfViewport = [mapSize[0] / 2, mapSize[1] / 2];
+      map.getView().centerOn(featureCoordinate, mapSize, centerOfViewport);
+
+      const content = `<p>${featureData.cinema.name}</p>
          <p>Adres: ${featureData.cinema.address}</p>
-         <p>Kontakt: ${featureData.cinema.contact.phone}, ${featureData.cinema.contact.email}</p>
-         <p>Link do repertuaru: <a href="${featureData.cinema.schedule_link}" target="_blank">${featureData.cinema.schedule_link}</a></p>
-         <p>Czy jest technologia IMAX?: ${featureData.cinema.imax_technology ? 'Tak' : 'Nie'}</p> 
+         <p>Kontakt: ${featureData.cinema.contact.phone}, ${
+        featureData.cinema.contact.email
+      }</p>
+         <p>Link do repertuaru: <a href="${
+           featureData.cinema.schedule_link
+         }" target="_blank">${featureData.cinema.schedule_link}</a></p>
+         <p>Czy jest technologia IMAX?: ${
+           featureData.cinema.imax_technology ? "Tak" : "Nie"
+         }</p> 
         `;
 
-        popupOverlay.setPosition(feature.getGeometry().getCoordinates());
-        document.getElementById('popup-content').innerHTML = content;
-      } else {
-        popupOverlay.setPosition(undefined);
-      }
-    });
+      popupOverlay.setPosition(featureCoordinate);
+      document.getElementById("popup-content").innerHTML = content;
+    } else {
+      popupOverlay.setPosition(undefined);
+    }
+  });
 }
