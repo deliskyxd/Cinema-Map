e280f6bf4000757855298f75c14a96dda16495a8
Voosaki <kamil.szczukowski@interia.pl>
2024-01-12 10:13:29 +0100

    Fix cinemas to work with new json structure

--- cinemas.js
+++ cinemas.js
@@ -1,27 +1,28 @@
-import {Feature} from 'ol';
-import {Icon, Style} from 'ol/style';
-import {fromLonLat} from 'ol/proj';
-import Point from 'ol/geom/Point';
-import VectorSource from 'ol/source/Vector';
+import { Feature } from "ol";
+import { Icon, Style } from "ol/style";
+import { fromLonLat } from "ol/proj";
+import Point from "ol/geom/Point";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
 
-export default function createCinemaPoints(map, cinemaLayer, jsonFilePath) {
-    fetch(jsonFilePath)
-    .then(response => response.json())
-    .then(cinemasData => {
-      const cinemaFeatures = cinemasData.cinemas.map((cinema) => {
+export default async function createCinemaLayers(map, jsonFilePath) {
+  const cinemaLayers = {};
+  try {
+    const response = await fetch(jsonFilePath);
+    const cinemasData = await response.json();
+    const layerNames = Object.keys(cinemasData);
 
-        let logoImage = './images/';
+    layerNames.forEach((layerName) => {
+      cinemaLayers[layerName] = new VectorLayer();
+      map.addLayer(cinemaLayers[layerName]);
 
-        if (cinema.name.toLowerCase().includes('cinemacity')) {
-          logoImage += 'cinemacitylogo.jpg';
-        } else if (cinema.name.toLowerCase().includes('helios')) {
-          logoImage += 'helioslogo.jpg';
-        } else if (cinema.name.toLowerCase().includes('multikino')) {
-          logoImage += 'multikinologo.jpg';
-        }
+      const cinemaFeatures = cinemasData[layerName].map((cinema) => {
+        let logoImage = `./images/${layerName}logo.jpg`;
 
         const point = new Feature({
-          geometry: new Point(fromLonLat([cinema.location.longitude, cinema.location.latitude])),
+          geometry: new Point(
+            fromLonLat([cinema.location.longitude, cinema.location.latitude])
+          ),
           cinema: cinema,
         });
 
@@ -29,8 +30,8 @@ export default function createCinemaPoints(map, cinemaLayer, jsonFilePath) {
           new Style({
             image: new Icon({
               anchor: [0.5, 46],
-              anchorXUnits: 'fraction',
-              anchorYUnits: 'pixels',
+              anchorXUnits: "fraction",
+              anchorYUnits: "pixels",
               src: logoImage,
               scale: 0.12,
             }),
@@ -44,7 +45,13 @@ export default function createCinemaPoints(map, cinemaLayer, jsonFilePath) {
         features: cinemaFeatures,
       });
 
-      cinemaLayer.setSource(cinemaSource);
-    })
-    .catch((error) => console.error('Error fetching JSON file:', error));
+      cinemaLayers[layerName].setSource(cinemaSource);
+    });
+
+    return cinemaLayers;
+  } catch (error) {
+    console.error("Error fetching JSON file:", error);
+
+    throw error;
+  }
 }
--- main.js
+++ main.js
@@ -1,50 +1,61 @@
-import './style.css';
-import {Map, View, Feature, Overlay} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import {fromLonLat, transformExtent} from 'ol/proj';
-import Point from 'ol/geom/Point';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import {Icon, Style} from 'ol/style';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { fromLonLat, transformExtent } from "ol/proj";
+import createCinemaLayers from "./cinemas";
+import createPopup from "./popup";
 
-import createCinemaPoints from './cinemas';
-import createPopup from './popup';
-
-//import cinemas from "./json/cinemas.json" assert { type: 'json' };
-//console.log(cinemas);
-
-const borders = transformExtent([9.1730000, 46.2100000, 29.6310000,58.2790000],'EPSG:4326', 'EPSG:3857');
-const polandCoordinates = fromLonLat([20,52]);
-const mapContainer = document.getElementById('map');
-
-const jsonFilePath = './data/cinemas.json';
-const cinemaLayer = new VectorLayer();
+const borders = transformExtent(
+  [9.173, 46.21, 29.631, 58.279],
+  "EPSG:4326",
+  "EPSG:3857"
+);
+const polandCoordinates = fromLonLat([20, 52]);
+const mapContainer = document.getElementById("map");
 
 const map = new Map({
-    target: mapContainer,
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        }),
-    ],
-    view: new View({
-        center: polandCoordinates, // center
-        extent: borders,
-        resolution: 3000,
-        zoom: 1,
-    })
+  target: mapContainer,
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: polandCoordinates,
+    extent: borders,
+    resolution: 3000,
+    zoom: 1,
+  }),
 });
 // If over an icon, change the cursor to a pointer
-map.on('pointermove', function (e) {
-    const pixel = map.getEventPixel(e.originalEvent);
-    const hit = map.hasFeatureAtPixel(pixel);
+map.on("pointermove", function (e) {
+  const pixel = map.getEventPixel(e.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
 
-    map.getTarget().style.cursor = hit ? 'pointer' : '';
+  map.getTarget().style.cursor = hit ? "pointer" : "";
 });
 
-map.addLayer(cinemaLayer);
-createCinemaPoints(map, cinemaLayer, jsonFilePath);
-createPopup(map);
+const jsonFilePath = "./data/cinemas.json";
 
-console.log("test");
+let cinemaLayers;
+
+async function initializeMap() {
+  cinemaLayers = await createCinemaLayers(map, jsonFilePath);
+  createPopup(map);
+}
+
+function toggleLayer(layerName) {
+  const cinemaLayer = cinemaLayers[layerName];
+  const layers = map.getLayers().getArray();
+
+  layers.includes(cinemaLayer)
+    ? map.removeLayer(cinemaLayer)
+    : map.addLayer(cinemaLayer);
+}
+
+initializeMap()
+  .then(() => {
+    toggleLayer("helios");
+  })
+  .catch((error) => console.error("Error initializing map:", error));
