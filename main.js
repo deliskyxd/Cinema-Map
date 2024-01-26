18e0277ff0a7e3b68861db7131faaef04a998019
Woosaki <kamil.szczukowski@interia.pl>
2024-01-26 19:05:30 +0100

    Move event listeners to after the data is fetched

--- main.js
+++ main.js
@@ -9,33 +9,33 @@ import { currentLocation, initializeGeolocation } from "./geolocation";
 import { toggleLayer, findNearestCinemas } from "./functions";
 
 const borders = transformExtent(
-    [9.173, 46.21, 29.631, 58.279],
-    "EPSG:4326",
-    "EPSG:3857"
+  [9.173, 46.21, 29.631, 58.279],
+  "EPSG:4326",
+  "EPSG:3857"
 );
 const polandCoordinates = fromLonLat([20, 52]);
 const mapContainer = document.getElementById("map");
 
 const map = new Map({
-    target: mapContainer,
-    layers: [
-        new TileLayer({
-            source: new OSM(),
-        }),
-    ],
-    view: new View({
-        center: polandCoordinates,
-        extent: borders,
-        resolution: 3000,
-        zoom: 1,
+  target: mapContainer,
+  layers: [
+    new TileLayer({
+      source: new OSM(),
     }),
+  ],
+  view: new View({
+    center: polandCoordinates,
+    extent: borders,
+    resolution: 3000,
+    zoom: 1,
+  }),
 });
 // If over an icon, change the cursor to a pointer
-map.on("pointermove", function(e) {
-    const pixel = map.getEventPixel(e.originalEvent);
-    const hit = map.hasFeatureAtPixel(pixel);
+map.on("pointermove", function (e) {
+  const pixel = map.getEventPixel(e.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
 
-    map.getTarget().style.cursor = hit ? "pointer" : "";
+  map.getTarget().style.cursor = hit ? "pointer" : "";
 });
 
 const jsonFilePath = "./data/cinemas.json";
@@ -43,28 +43,35 @@ const jsonFilePath = "./data/cinemas.json";
 let cinemaLayers;
 let allCinemas = [];
 async function initializeMap() {
-    const result = await createCinemaLayers(map, jsonFilePath);
-    cinemaLayers = result.cinemaLayers;
-    allCinemas = result.allCinemas;
-    createPopup(map);
-    initializeGeolocation(map);
+  const result = await createCinemaLayers(map, jsonFilePath);
+  cinemaLayers = result.cinemaLayers;
+  allCinemas = result.allCinemas;
+  createPopup(map);
+  initializeGeolocation(map);
+
+  document
+    .getElementById("findNearestCinemas")
+    .addEventListener("click", () =>
+      findNearestCinemas(map, currentLocation, allCinemas, 5)
+    );
+
+  document
+    .getElementById("toggleCinemaCity")
+    .addEventListener("click", () =>
+      toggleLayer(map, cinemaLayers, "cinemacity")
+    );
+
+  document
+    .getElementById("toggleHelios")
+    .addEventListener("click", () => toggleLayer(map, cinemaLayers, "helios"));
+
+  document
+    .getElementById("toggleMultikino")
+    .addEventListener("click", () =>
+      toggleLayer(map, cinemaLayers, "multikino")
+    );
 }
 
 initializeMap().catch((error) =>
-    console.error("Error initializing map:", error)
+  console.error("Error initializing map:", error)
 );
-
-document
-    .getElementById("findNearestCinemas")
-    .addEventListener("click", () => findNearestCinemas(map, currentLocation, allCinemas, 5));
-document
-    .getElementById("toggleCinemaCity")
-    .addEventListener("click", () =>
-        toggleLayer(map, cinemaLayers, "cinemacity")
-    );
-document
-    .getElementById("toggleHelios")
-    .addEventListener("click", () => toggleLayer(map, cinemaLayers, "helios"));
-document
-    .getElementById("toggleMultikino")
-    .addEventListener("click", () => toggleLayer(map, cinemaLayers, "multikino"));
