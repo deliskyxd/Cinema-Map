f84efe8de7b5b2a3031caa72cb175c16ad74a0e3
Woosaki <kamil.szczukowski@interia.pl>
2024-05-11 05:51:53 +0200

    Add clustering for cinemas.js

--- cinemas.js
+++ cinemas.js
@@ -1,8 +1,15 @@
 import { Feature } from "ol";
-import { Icon, Style } from "ol/style";
+import {
+  Icon,
+  Style,
+  Circle as CircleStyle,
+  Stroke,
+  Text,
+  Fill,
+} from "ol/style";
 import { fromLonLat } from "ol/proj";
 import Point from "ol/geom/Point";
-import VectorSource from "ol/source/Vector";
+import { Vector as VectorSource, Cluster } from "ol/source";
 import VectorLayer from "ol/layer/Vector";
 
 const createCinemaLayers = async (map, jsonFilePath) => {
@@ -18,26 +25,12 @@ const createCinemaLayers = async (map, jsonFilePath) => {
       map.addLayer(cinemaLayers[layerName]);
 
       const cinemaFeatures = cinemasData[layerName].map((cinema) => {
-        let logoImage = `./images/${layerName}logo.png`;
-
         const point = new Feature({
           geometry: new Point(
             fromLonLat([cinema.location.longitude, cinema.location.latitude])
           ),
           cinema: cinema,
         });
-
-        point.setStyle(
-          new Style({
-            image: new Icon({
-              anchor: [0.5, 46],
-              anchorXUnits: "fraction",
-              anchorYUnits: "pixels",
-              src: logoImage,
-              scale: 0.05,
-            }),
-          })
-        );
         allCinemas.push(point);
 
         return point;
@@ -47,7 +40,45 @@ const createCinemaLayers = async (map, jsonFilePath) => {
         features: cinemaFeatures,
       });
 
-      cinemaLayers[layerName].setSource(cinemaSource);
+      const clusterSource = new Cluster({
+        distance: 50,
+        source: cinemaSource,
+      });
+
+      cinemaLayers[layerName] = new VectorLayer({
+        source: clusterSource,
+        style: function (feature) {
+          const size = feature.get("features").length;
+          let style = feature.get("features")[0].getStyle();
+
+          if (size > 1) {
+            style = new Style({
+              image: new CircleStyle({
+                radius: 20,
+                stroke: new Stroke({ color: "#fff" }),
+                fill: new Fill({ color: "#3399CC" }),
+              }),
+              text: new Text({
+                text: size.toString(),
+                fill: new Fill({ color: "#fff" }),
+              }),
+            });
+          } else {
+            style = new Style({
+              image: new Icon({
+                anchor: [0.5, 46],
+                anchorXUnits: "fraction",
+                anchorYUnits: "pixels",
+                src: `./images/${layerName}logo.png`,
+                scale: 0.05,
+              }),
+            });
+          }
+          return style;
+        },
+      });
+
+      map.addLayer(cinemaLayers[layerName]);
     });
 
     return { cinemaLayers, allCinemas };
--- main.js
+++ main.js
@@ -9,7 +9,14 @@ import { currentLocation, initializeGeolocation } from "./geolocation";
 import { toggleLayer, findNearestCinemas } from "./functions";
 import { Cluster, Vector as VectorSource } from "ol/source";
 import VectorLayer from "ol/layer/Vector";
-import { Circle as CircleStyle, Fill, Stroke, Style, Text } from "ol/style";
+import {
+  Circle as CircleStyle,
+  Fill,
+  Stroke,
+  Style,
+  Text,
+  Icon,
+} from "ol/style";
 
 const borders = transformExtent(
   [9.173, 46.21, 29.631, 58.279],
@@ -77,40 +84,6 @@ async function initializeMap() {
   document.getElementById("close-button").addEventListener("click", () => {
     document.getElementById("cinema-list-container").style.display = "none";
   });
-
-  // Cluster cinemas
-  const clusterSource = new Cluster({
-    distance: 40,
-    source: new VectorSource({ features: allCinemas }),
-  });
-
-  const styleCache = {};
-  const clusters = new VectorLayer({
-    source: clusterSource,
-    style: function (feature) {
-      const size = feature.get("features").length;
-      let style = styleCache[size];
-      if (!style) {
-        style = new Style({
-          image: new CircleStyle({
-            radius: 20,
-            stroke: new Stroke({ color: "#fff" }),
-            fill: new Fill({
-              color: "#3399CC",
-            }),
-          }),
-          text: new Text({
-            text: size.toString(),
-            fill: new Fill({ color: "#fff" }),
-          }),
-        });
-        styleCache[size] = style;
-      }
-      return style;
-    },
-  });
-
-  map.addLayer(clusters);
 }
 
 initializeMap().catch((error) =>
